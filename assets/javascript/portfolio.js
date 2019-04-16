bad3a1de2ddda38b1697ebed33a5d4a6b93e7639
bradleyjd19 <bradleyjd19@gmail.com>
2019-04-16 13:02:44 -0500

    updated tagline

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,22 +1,11 @@
 window.onload = function () {
-  window.setTimeout(fadein1, 1000);
-  window.setTimeout(fadein2, 3000);
-  window.setTimeout(fadein3, 5000);
-  window.setTimeout(fadein4, 7000);
+  setTimeout(function() {fadein("tagline1")}, 1500);
+  setTimeout(function() {fadein("tagline2")}, 3000);
+  setTimeout(function() {fadein("tagline3")}, 4500);
+  setTimeout(function() {fadein("tagline4")}, 6500);
+  setTimeout(function() {fadein("intro")}, 8500);  
 }
 
-function fadein1() {
-  document.getElementById("tagline1").style.opacity = "1";
-}
-
-function fadein2() {
-  document.getElementById("tagline2").style.opacity = "1";
-}
-
-function fadein3() {
-  document.getElementById("tagline3").style.opacity = "1";
-}
-
-function fadein4() {
-  document.getElementById("intro").style.opacity = "1";
+function fadein(line) {
+  document.getElementById(line).style.opacity = "1";
 }
