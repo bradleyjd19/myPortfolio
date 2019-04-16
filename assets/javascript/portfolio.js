09b0a74ff2c3088fcacd8aa86b580c4a7623ed77
bradleyjd19 <bradleyjd19@gmail.com>
2019-04-15 23:04:40 -0500

    added fade function

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,19 +1,22 @@
+window.onload = function () {
+  window.setTimeout(fadein1, 1000);
+  window.setTimeout(fadein2, 3000);
+  window.setTimeout(fadein3, 5000);
+  window.setTimeout(fadein4, 7000);
+}
 
+function fadein1() {
+  document.getElementById("tagline1").style.opacity = "1";
+}
 
-  window.onload = function () {
-    window.setTimeout(fadein1, 2000);
-    window.setTimeout(fadein2, 4000);
-    window.setTimeout(fadein3, 6000);
-  }
+function fadein2() {
+  document.getElementById("tagline2").style.opacity = "1";
+}
 
-  function fadein1() {
-    document.getElementById('tagline1').style.opacity = '1';
-  }
+function fadein3() {
+  document.getElementById("tagline3").style.opacity = "1";
+}
 
-  function fadein2() {
-    document.getElementById('tagline2').style.opacity = '1';
-  }
-
-  function fadein3() {
-    document.getElementById('tagline3').style.opacity = '1';
-  }
+function fadein4() {
+  document.getElementById("intro").style.opacity = "1";
+}
