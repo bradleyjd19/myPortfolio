8a09c0c3ad05e4a09fc307b80292005e3c9b7d57
Joshua Bradley <bradleyjd19@gmail.com>
2019-04-17 09:49:59 -0500

    faded intro line

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -3,9 +3,21 @@ window.onload = function () {
   setTimeout(function() {fadein("tagline2")}, 3000);
   setTimeout(function() {fadein("tagline3")}, 4500);
   setTimeout(function() {fadein("tagline4")}, 6500);
-  setTimeout(function() {fadein("intro")}, 8500);  
+  setTimeout(function() {fadein("preName")}, 8500);
+  setTimeout(function() {fadein("intro")}, 8500);
+  setTimeout(function() {fadein("postName")}, 8500);
+  setTimeout(fadeout, 12000);
 }
 
 function fadein(line) {
   document.getElementById(line).style.opacity = "1";
+}
+
+function fadeout() {
+  document.getElementById("tagline1").style.opacity = "0";
+  document.getElementById("tagline2").style.opacity = "0";
+  document.getElementById("tagline3").style.opacity = "0";
+  document.getElementById("tagline4").style.opacity = "0";
+  document.getElementById("preName").style.opacity = "0";
+  document.getElementById("postName").style.opacity = "0";
 }
