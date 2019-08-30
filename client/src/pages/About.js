aa692ded5143f3d16cde31a8a692a82607a0df99
Joshua Bradley <bradleyjd19@gmail.com>
2019-08-30 00:20:41 -0500

    working on mobile responsiveness

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,8 +10,11 @@ class About extends Component {
         <div className="container" id="aboutContainer">
           <div className="row">
             <div className="column">
+              <img id="bioPic" src={require("../images/selfie.jpg")} alt="Joshua D. Bradley" />
+
               <p id="bio">Joshua D. Bradley is a Full-Stack Developer specializing in UI/UX, with experience in React, JavaScript, Node, Express, SQL, and Mongo.</p>
-              <p id="bio">As a former laboratory management professional, he brings strong analytical and problem-solving skills to the world of coding, along with the ability to adapt to the ever-changing needs inherent in any situation.</p>
+
+              <p id="bio2">As a former laboratory management professional, he brings strong analytical and problem-solving skills to the world of coding, along with the ability to adapt to the ever-changing needs inherent in any situation.</p>
             </div>
           </div>
         </div>
